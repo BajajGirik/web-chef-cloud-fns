cadc050bbfc73616a6fb660b37148c9fe00c77b0
Girik Bajaj <bajajgirik2010@gmail.com>
2022-01-10 22:05:47 +0530

    Added updateCartAmountOnCreate

---
+++ functions/src/index.ts
@@ -0,0 +1,37 @@
+import * as functions from "firebase-functions";
+import * as admin from "firebase-admin";
+// Start writing Firebase Functions
+// https://firebase.google.com/docs/functions/typescript
+
+// export const helloWorld = functions.https.onRequest((request, response) => {
+//   functions.logger.info("Hello logs!", {structuredData: true});
+//   response.send("Hello from Firebase!");
+// });
+admin.initializeApp();
+
+export const updateCartAmountOnCreate = functions.firestore
+  .document("users/{userId}/cart/{productId}")
+  .onCreate(async (snapshot, context) => {
+    const data = snapshot.data();
+    if (data) {
+      try {
+        const productId = context.params.productId;
+        const productSnap = await admin.firestore().doc(`products/${productId}`).get();
+        if (productSnap?.data()?.price === undefined) 
+            return null;
+          
+        let amount = productSnap!.data()!.price * data.qty;
+        
+        const userRef = snapshot.ref.parent.parent;
+        const userSnap = await userRef?.get();
+        amount = (userSnap!.data()?.amount ? userSnap!.data()?.amount : 0) + amount;
+        
+        return userRef!.set({ amount: amount }, { merge: true });
+          
+      } catch (err) {
+          console.log(err);
+      }
+    }
+
+    return null;
+  });
